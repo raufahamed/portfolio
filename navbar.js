9ffb994cd00d509f159a7395ed99e449b954c317
rawoof <rawoofahamed456@gmail.com>
2024-08-29 19:34:33 +0530

    -

--- navbar.js
+++ navbar.js
@@ -1,32 +1,26 @@
+(function ($) {
+  "use strict";
 
-
-
-
-(function($) { "use strict";
-		
-
-
-	var app = function () {
-		var body = undefined;
-		var menu = undefined;
-		var menuItems = undefined;
-		var init = function init() {
-			body = document.querySelector('body');
-			menu = document.querySelector('.menu-icon');
-			menuItems = document.querySelectorAll('.nav__list-item');
-			applyListeners();
-		};
-		var applyListeners = function applyListeners() {
-			menu.addEventListener('click', function () {
-				return toggleClass(body, 'nav-active');
-			});
-		};
-		var toggleClass = function toggleClass(element, stringClass) {
-			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-		};
-		init();
-	}();
-
-         
-              
-})(jQuery); 
+  var app = (function () {
+    var body = undefined;
+    var menu = undefined;
+    var menuItems = undefined;
+    var init = function init() {
+      body = document.querySelector("body");
+      menu = document.querySelector(".menu-icon");
+      menuItems = document.querySelectorAll(".nav__list-item");
+      applyListeners();
+    };
+    var applyListeners = function applyListeners() {
+      menu.addEventListener("click", function () {
+        return toggleClass(body, "nav-active");
+      });
+    };
+    var toggleClass = function toggleClass(element, stringClass) {
+      if (element.classList.contains(stringClass))
+        element.classList.remove(stringClass);
+      else element.classList.add(stringClass);
+    };
+    init();
+  })();
+})(jQuery);
